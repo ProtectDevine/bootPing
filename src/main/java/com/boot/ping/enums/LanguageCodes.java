703174879a15c3f5e63f9ae6f6748e0f0d21c2bb
ShinKyungHo <skh3235@gmail.com>
2025-03-22 21:09:45 +0900

    FEATURE-001 다국어 처리 코드 수정

--- src/main/java/com/boot/ping/enums/LanguageCodes.java
+++ src/main/java/com/boot/ping/enums/LanguageCodes.java
@@ -3,6 +3,8 @@ package com.boot.ping.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+
 @Getter
 @RequiredArgsConstructor
 public enum LanguageCodes {
@@ -11,5 +13,16 @@ public enum LanguageCodes {
     ENGLISH("en");
 
     private final String language;
+
+    public static LanguageCodes getLanguageCode(String language) {
+
+        return Arrays.stream(LanguageCodes.values())
+                .filter(x -> x.getLanguage()
+                .equals(language)).findFirst()
+                .orElseThrow(() -> new RuntimeException());
+
+    }
+
+
 
 }
